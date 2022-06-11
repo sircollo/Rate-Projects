cb961639acfeaf562c40bfab7785daa1280831e8
Collins Sirwani <sircollo71@gmail.com>
2022-06-12 02:52:49 +0300

    Fix: this will change imagefields to cloudinary fields

--- projectsapp/models.py
+++ projectsapp/models.py
@@ -1,18 +1,27 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from django.urls import reverse
+from cloudinary.models import CloudinaryField
+import cloudinary
+
 # Create your models here.
 
 class Profile(models.Model):
   name = models.CharField(max_length=30)
   bio = models.TextField()
-  avatar = models.ImageField(upload_to='avatars/')
+  # avatar = models.ImageField(upload_to='avatars/',default='image')
+  avatar = CloudinaryField('image')
   country = models.CharField(max_length=30)
   user = models.OneToOneField(User, on_delete=models.CASCADE,null=True)
   
   
   def __str__(self):
-    return self.name
+    return self.user.username
+  
+  def get_absolute_url(self):
+    return reverse('index')
   
 class Project(models.Model):
   name = models.CharField(max_length=30)
@@ -37,6 +46,7 @@ class Project(models.Model):
     choices= LANGUAGES,
     default= HTML,
   )
+  poster = cloudinary.models.CloudinaryField('image',default='image')
   upload_date = models.DateTimeField(auto_now_add=True)
   user = models.ForeignKey(Profile, on_delete=models.CASCADE,null=True,related_name='project')
   
