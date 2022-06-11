0629c9b9b12f5be47958ef59b8fbf1dfd4af2426
Collins Sirwani <sircollo71@gmail.com>
2022-06-11 22:31:38 +0300

    Feat: this will create url paths for signup, signin and logout

--- projectsapp/urls.py
+++ projectsapp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path,include,re_path as url
 from . import views
+from .views import *
 
 urlpatterns = [
-    url('$', views.home, name='index')
+    url('^$', views.home, name='index'),
+    url('^signup/', views.signup, name='signup'),
+    url('^signin/', views.signin, name='signin'),
+    url('logout/', SignOutView.as_view(), name='logout'),
 ]
