30e53dda4f541d3b4addad785d0c566fccc6486a
Collins Sirwani <sircollo71@gmail.com>
2022-06-12 02:49:30 +0300

    Feat: this will create url paths for profile and profile update

--- projectsapp/urls.py
+++ projectsapp/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path,include,re_path as url
 from . import views
 from .views import *
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     url('^$', views.home, name='index'),
     url('^signup/', views.signup, name='signup'),
     url('^signin/', views.signin, name='signin'),
     url('logout/', SignOutView.as_view(), name='logout'),
+    url('^profile/(\d+)', views.profile, name='profile'),
+    path('update-profile/<str:id>', views.updateProfile, name='update_profile'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
