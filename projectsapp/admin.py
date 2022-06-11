55e533b9fea2dbbf86e8d3fdcae10d693277e050
Collins Sirwani <sircollo71@gmail.com>
2022-06-11 21:13:49 +0300

    Feat: register model classes for admin view

--- projectsapp/admin.py
+++ projectsapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from projectsapp.models import Profile
+from .models import *
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
