aae4074ec40b25a82ee3d54a968dec0849154340
Collins Sirwani <sircollo71@gmail.com>
2022-06-12 02:01:53 +0300

    Register signals.py

--- projectsapp/apps.py
+++ projectsapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ProjectsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projectsapp'
+    
+    def ready(self):
+        import projectsapp.signals
