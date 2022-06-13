3735b9c9639fc6d19250fe3451ab211ceb08054b
Collins Sirwani <sircollo71@gmail.com>
2022-06-13 11:29:31 +0300

    Add project and rating serializer classes

--- projectsapp/serializer.py
+++ projectsapp/serializer.py
@@ -5,3 +5,13 @@ class ProfileSerializer(serializers.ModelSerializer):
   class Meta:
     model = Profile
     fields = ('id', 'name', 'bio','avatar','country','user')
+    
+class ProjectsSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Project
+    fields = ('name','description','poster','language','url','category','total_ratings','design_average','usability_average','content_average')
+    
+class RatingSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Rating
+    fields = '__all__'
